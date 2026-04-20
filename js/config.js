e7613128937e4b48aa1f2bd8692993184f510734
Pappu Baraf <pappurbaraf@gmail.com>
2026-04-20 13:40:44 +0530

    Refactor config.js for improved API documentation
    Updated the config structure for clarity on API Gateway and Lambda usage.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_yshRByLPT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7h69n0giho880o6g9fhdehpkud', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_yshRByLPT',
+        userPoolClientId: '7h69n0giho880o6g9fhdehpkud',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''   // Leave empty for now. We'll fill this after creating API Gateway + Lambda
     }
 };
